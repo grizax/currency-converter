18a49d48587b3b6dcaa5949cbef3f6022ebfdc5a
grizax <ndbfoster@gmail.com>
2015-01-21 00:38:19 -0500

    Conversion created with basic tests run thru

---
+++ currency.py
@@ -0,0 +1,17 @@
+#!/bin/python
+
+rates = [("USD", "EUR", 0.86), ("EUR", "JPY", 136.28)]
+
+
+def convert(rates, value, from_a, to_b):
+    """Convert one currency to another using the current exchange rate"""
+    if from_a == to_b:
+        return value
+    else:
+        new_list = [(start, end, rate)
+                    for (start, end, rate)
+                    in rates if from_a == start]
+        elput = new_list[0]
+        new_rate = elput[2]
+        new_value = new_rate * value
+        return new_value
---
+++ test_currency.py
@@ -0,0 +1,10 @@
+#!/bin/python
+import currency as cy
+
+rates = [("USD", "EUR", 0.86), ("EUR", "JPY", 137.05)]
+
+def test_dollar_for_dollar():
+    assert cy.convert(rates, 1, "USD", "USD") == 1
+
+def test_conversion():
+    assert cy.convert(rates, 1, "USD", "EUR") == .86
